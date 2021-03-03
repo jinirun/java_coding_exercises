1dca9accd4bd13eefd101c2fb6f9573e30ac6ad0
Ranjini <ranj.jini@gmail.com>
2021-03-03 12:26:30 +0000

    Implemented checkIsFromManchester in Exercise002

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,13 +2,15 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
+    public boolean isFromManchester(Person person) {
+        if (person.getCity() == "Manchester")
+            return true;
         return false;
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
+    public boolean canWatchFilm(Person person, int ageLimit) {
         // Add your code here
+
         return false;
     }
     
